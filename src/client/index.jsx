1c8c264d47e77e744d09f86a305d73da8a72efb0
upieez <samuelhww@gmail.com>
2020-02-27 09:58:54 +0800

    done till mvp

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,11 +3,55 @@ import ReactDOM from 'react-dom';
 
 import tweets from 'tweets'
 
+class UrlEntity extends React.Component {
+  render() {
+    
+    return(
+      <div>{this.props.urlentity.url}</div>
+    );
+  }
+}
+class Entities extends React.Component {
+  render() {
+    
+    return(
+      <div><UrlEntity urlentity={this.props.entities.url.urls[0]}/></div>
+
+    );
+  }
+}
+class User extends React.Component {
+  render() {
+    
+    return(
+      <div>
+      <div><Entities entities={this.props.user.entities}/></div>
+      <div>{this.props.user.name}</div>
+      </div>
+    );
+  }
+}
+
+class Tweet extends React.Component {
+  render() {
+    console.log(this.props.tweet);
+    return(
+      <div>
+      <div><User user={this.props.tweet.user}/></div>
+      <div>{this.props.tweet.text}</div>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
+    var twitter = tweets.tweets.map(function(tweet) {
+      return <Tweet tweet={tweet}/>
+    })
     return (
       <div>
-        tweets
+      {twitter}
       </div>
     );
   }
